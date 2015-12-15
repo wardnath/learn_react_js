b34d26a652f1d38810d17ac6f33cf57e13f8b3fa
Nathan Ward <nathan.ward@swipesense.com>
2015-12-15 14:10:20 -0600

    finished module 14

--- scripts/main.js
+++ scripts/main.js
@@ -45,12 +45,12 @@ var App = React.createClass({
     return (
       <div className="catch-of-the-day">
         <div className = "menu">
-          <Header tagline="Fresh Seafood Market" />
+          <Header tagline="Fresh Seafood  Market" />
           <ul className="list-of-fishes">
             {Object.keys(this.state.fishes).map(this.renderFish)}
           </ul>
         </div>
-        <Order/>
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
       )
@@ -156,9 +156,45 @@ var Header = React.createClass({
 */
 
 var Order = React.createClass({
+  renderOrder : function(key) {
+    var fish = this.props.fishes[key];
+    var count = this.props.order[key];
+
+    if(!fish) {
+      return <li key={key}>Sorry, fish no longer available!</li>
+    }
+    return (<li>
+        {count}lbs
+        {fish.name}
+        <span className="price">{h.formatPrice(count * fish.price)}</span>
+      </li>)
+  },
   render : function() {
+    var orderIds = Object.keys(this.props.order);
+
+    var total = orderIds.reduce((prevTotal, key)=> {
+      var fish = this.props.fishes[key];
+      var count = this.props.order[key];
+      var isAvailable = fish && fish.status === 'available';
+
+      if(fish && isAvailable) {
+        return prevTotal + (count * parseInt(fish.price) || 0);
+      }
+
+      return prevTotal;
+    }, 0);
+
     return (
-      <p>Order</p>
+      <div className="order-wrap">
+        <h2 className="order-title">Your Order</h2>
+        <ul className="order">
+          {orderIds.map(this.renderOrder)}
+          <li className="total">
+            <strong>Total:</strong>
+            {h.formatPrice(total)}
+          </li>
+        </ul>
+      </div>
     )
   }
 });
@@ -188,7 +224,7 @@ var Inventory = React.createClass({
 var StorePicker = React.createClass({
   mixins : [History],
   goToStore : function(event) {
-    event.preventDefault(); 
+    event.preventDefault();
     // get the data from the input
     var storeId = this.refs.storeId.value;
     this.history.pushState(null, '/store/' + storeId);
