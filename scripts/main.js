325d8a5bf9e24e9e866ba89a1eb62213cfffaf0a
Nathan Ward <nathan.ward@swipesense.com>
2015-12-16 10:24:54 -0600

    finished 17

--- scripts/main.js
+++ scripts/main.js
@@ -14,11 +14,14 @@ var h = require('./helpers');
 var Rebase = require('re-base');
 var base = Rebase.createClass('https://learning-app-nate-ward.firebaseio.com/');
 
+var Catalyst = require('react-catalyst')
+
 /*
   App
 */
 
 var App = React.createClass({
+  mixins : [Catalyst.LinkedStateMixin],
   getInitialState : function () {
     return {
       fishes : {},
@@ -75,7 +78,7 @@ var App = React.createClass({
           </ul>
         </div>
         <Order fishes={this.state.fishes} order={this.state.order} />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState} />
       </div>
       )
   }
@@ -229,10 +232,27 @@ var Order = React.createClass({
 */
 
 var Inventory = React.createClass({
+  renderInventory : function(key) {
+    var linkState = this.props.linkState
+    return (
+      <div className="fish-edit" key={key}>
+        <input type="text" valueLink={linkState('fishes.' + key + '.name')} />
+        <input type="text" valueLink={linkState('fishes.' + key + '.price')} />
+        <select valueLink={linkState('fishes.' + key + '.status')}>
+          <option value="unavailable">Sold Out!</option>
+          <option value="available">Fresh!</option>
+        </select>
+        <textarea valueLink={linkState('fishes.' + key + '.name')}></textarea>
+        <input type="text" valueLink={linkState('fishes.' + key + '.image')} />
+        <button>Remove Fish</button>
+      </div>
+    )
+  },
   render : function() {
     return (
       <div>
         <h2>Inventory</h2>
+        {Object.keys(this.props.fishes).map(this.renderInventory)}
         <AddFishForm {...this.props} />
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
