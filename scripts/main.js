0f55e0df2f6eca267584b750444ae3e70fff5ea1
Nathan Ward <nathan.ward@swipesense.com>
2015-12-16 11:53:12 -0600

    finished 19

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var CSSTransitionGroup = require('react-addons-css-transition-group');
 
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
@@ -207,10 +208,14 @@ var Order = React.createClass({
       return <li key={key}>Sorry, fish no longer available! {removeButton}</li>
     }
     return (<li key={key}>
-        {count}lbs
-        {fish.name}
+        <CSSTransitionGroup component="span" transitionName="count"
+          transitionLeaveTimeout={250} transtionEnterTimeout={250}
+          className="count">
+          <span key={count}> {count} </span>
+        </CSSTransitionGroup>
+
+        lbs {fish.name} {removeButton}
         <span className="price">{h.formatPrice(count * fish.price)}</span>
-        {removeButton}
       </li>)
   },
   render : function() {
@@ -231,13 +236,21 @@ var Order = React.createClass({
     return (
       <div className="order-wrap">
         <h2 className="order-title">Your Order</h2>
-        <ul className="order">
+
+        <CSSTransitionGroup
+          className="order"
+          component="ul"
+          transitionName="order"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+        >
           {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>Total:</strong>
             {h.formatPrice(total)}
           </li>
-        </ul>
+        </CSSTransitionGroup>
+
       </div>
     )
   }
